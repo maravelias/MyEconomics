2b33d172c314da97048ece05a92b451afa5eb10b
Giorgos Maravelias <maravelias@netmechanics.gr>
2024-10-31 17:06:56 +0200

    Remove TraderInfoView and update SupplierInfoView
    Deleted TraderInfoView and its associated XML file. Updated SupplierInfoView to include necessary components previously in TraderInfoView, and adjusted the menu to reflect these changes.

--- src/main/java/gr/maravelias/myeconomics/view/supplierinfo/SupplierInfoView.java
+++ src/main/java/gr/maravelias/myeconomics/view/supplierinfo/SupplierInfoView.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 
 import com.vaadin.flow.router.Route;
 import gr.maravelias.myeconomics.entity.Trader;
+import gr.maravelias.myeconomics.view.invoicesfragment.InvoicesFragment;
 import gr.maravelias.myeconomics.view.main.MainView;
 import gr.maravelias.myeconomics.view.supplierstransactionsfragment.SuppliersTransactionsFragment;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.StandardView;
 import io.jmix.flowui.view.Subscribe;
 import io.jmix.flowui.view.Target;
+import io.jmix.flowui.view.ViewComponent;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 @ViewDescriptor("supplier-info-view.xml")
 public class SupplierInfoView extends StandardView {
 
-    @Autowired
-    private gr.maravelias.myeconomics.view.invoicesfragment.InvoicesFragment suppliersInvoicesFragment;
+    @ViewComponent
+    private InvoicesFragment suppliersInvoicesFragment;
 
-    @Autowired
+    @ViewComponent
     private SuppliersTransactionsFragment suppliersTransactionsFragment;
 
     @Subscribe(id = "tradersDc", target = Target.DATA_CONTAINER)
