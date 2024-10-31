741dc0fbf4721b83053d91824e2da8f9e6260966
Giorgos Maravelias <maravelias@netmechanics.gr>
2024-10-31 16:10:00 +0200

    Add SuppliersTransactionsFragment to trader info view
    Created `suppliers-transactions-fragment.xml` and `SuppliersTransactionsFragment.java` for managing supplier transactions. Integrated the new fragment into `trader-info-view.xml` to display relevant supplier transaction data.

---
+++ src/main/java/gr/maravelias/myeconomics/view/supplierstransactionsfragment/SuppliersTransactionsFragment.java
@@ -0,0 +1,24 @@
+package gr.maravelias.myeconomics.view.supplierstransactionsfragment;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import gr.maravelias.myeconomics.entity.BankTransaction;
+import io.jmix.flowui.fragment.Fragment;
+import io.jmix.flowui.fragment.FragmentDescriptor;
+import io.jmix.flowui.model.CollectionLoader;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.Target;
+import io.jmix.flowui.view.View;
+import io.jmix.flowui.view.ViewComponent;
+
+@FragmentDescriptor("suppliers-transactions-fragment.xml")
+public class SuppliersTransactionsFragment extends Fragment<VerticalLayout> {
+
+    @ViewComponent
+    protected CollectionLoader<BankTransaction> supplierTransactionsDl;
+
+    @Subscribe(target = Target.HOST_CONTROLLER)
+    public void onHostBeforeShow(View.BeforeShowEvent event) {
+        supplierTransactionsDl.load();
+    }
+
+}
