38dff56d703dc979ac2c166fc946df03d96e87fc
Giorgos Maravelias <maravelias@netmechanics.gr>
2024-10-31 16:20:20 +0200

    Add SuppliersTransactionsFragment to trader info view
    Created `suppliers-transactions-fragment.xml` and `SuppliersTransactionsFragment.java` for managing supplier transactions. Integrated the new fragment into `trader-info-view.xml` to display relevant supplier transaction data.

--- src/main/java/gr/maravelias/myeconomics/view/trader/TraderInfoView.java
+++ src/main/java/gr/maravelias/myeconomics/view/trader/TraderInfoView.java
@@ -3,9 +3,11 @@ package gr.maravelias.myeconomics.view.trader;
 import com.vaadin.flow.router.Route;
 import gr.maravelias.myeconomics.entity.Trader;
 import gr.maravelias.myeconomics.view.main.MainView;
+import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.view.DialogMode;
-import io.jmix.flowui.view.LookupComponent;
 import io.jmix.flowui.view.StandardListView;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.Target;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 
@@ -13,9 +15,15 @@ import io.jmix.flowui.view.ViewDescriptor;
 @Route(value = "tradersinfo", layout = MainView.class)
 @ViewController("myec_Trader.info")
 @ViewDescriptor("trader-info-view.xml")
-@LookupComponent("tradersDataGrid")
 @DialogMode(width = "64em")
 public class TraderInfoView extends StandardListView<Trader> {
 
+    @Subscribe(id = "tradersDc", target = Target.DATA_CONTAINER)
+    public void tradersDcItemChange(final InstanceContainer.ItemChangeEvent<Trader> event) {
+        if (event.getItem() != null) {
 
+            System.out.println("is not NUlL:"+ event.getItem().getTitle() );
+        }
+    }
 }
+
