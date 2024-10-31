1f7f43dc98c148ba926b595887b7240a2c8b42fc
Giorgos Maravelias <maravelias@netmechanics.gr>
2024-10-31 16:54:02 +0200

    Add SupplierInfoView for displaying supplier details
    This commit introduces the SupplierInfoView class and its corresponding XML descriptor for displaying supplier information. It integrates the InvoicesFragment and SuppliersTransactionsFragment to show relevant data based on the selected supplier. The changes also include updates to message properties and the main menu to reflect the new supplier info view feature.

--- src/main/java/gr/maravelias/myeconomics/view/supplierinfo/SupplierInfoView.java
+++ src/main/java/gr/maravelias/myeconomics/view/supplierinfo/SupplierInfoView.java
@@ -1,4 +1,5 @@
-package gr.maravelias.myeconomics.view.trader;
+package gr.maravelias.myeconomics.view.supplierinfo;
+
 
 import java.util.UUID;
 
@@ -7,20 +8,17 @@ import gr.maravelias.myeconomics.entity.Trader;
 import gr.maravelias.myeconomics.view.main.MainView;
 import gr.maravelias.myeconomics.view.supplierstransactionsfragment.SuppliersTransactionsFragment;
 import io.jmix.flowui.model.InstanceContainer;
-import io.jmix.flowui.view.DialogMode;
-import io.jmix.flowui.view.StandardListView;
+import io.jmix.flowui.view.StandardView;
 import io.jmix.flowui.view.Subscribe;
 import io.jmix.flowui.view.Target;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-@Route(value = "tradersinfo", layout = MainView.class)
-@ViewController("myec_Trader.info")
-@ViewDescriptor("trader-info-view.xml")
-@DialogMode(width = "64em")
-public class TraderInfoView extends StandardListView<Trader> {
+@Route(value = "supplier-info-view", layout = MainView.class)
+@ViewController("myec_SupplierInfoView")
+@ViewDescriptor("supplier-info-view.xml")
+public class SupplierInfoView extends StandardView {
 
     @Autowired
     private gr.maravelias.myeconomics.view.invoicesfragment.InvoicesFragment suppliersInvoicesFragment;
@@ -39,5 +37,5 @@ public class TraderInfoView extends StandardListView<Trader> {
             suppliersTransactionsFragment.setTraderId(traderId);
         }
     }
-}
 
+}
