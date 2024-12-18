1e2b198650f9f4270e6bd8099b483218d22d3e35
Giorgos Maravelias <maravelias@netmechanics.gr>
2024-12-18 16:57:40 +0200

    Add TotalsView feature to menu and security policies
    Introduced a new TotalsView screen with related XML, Java, and message files. Updated the menu, messages, and security policies to register and enable access to this view. This feature provides a comprehensive dashboard as part of the application.

--- src/main/java/gr/maravelias/myeconomics/security/UiMinimalRole.java
+++ src/main/java/gr/maravelias/myeconomics/security/UiMinimalRole.java
@@ -25,7 +25,7 @@ public interface UiMinimalRole {
 
     String CODE = "ui-minimal";
 
-    @ViewPolicy(viewIds = {"myec_MainView", "myec_Trader.list", "myec_BankAccount.list", "myec_BankAccount.detail", "flowui_AddConditionView", "flowui_GroupFilterCondition.detail", "flowui_PropertyFilterCondition.detail", "flowui_JpqlFilterCondition.detail", "myec_BankTransaction.list", "myec_BankTransaction.detail", "myec_ExpenseInvoice.list", "myec_ExpenseInvoice.detail", "myec_ViewTraderBalances.list", "myec_SupplierInfoView"})
+    @ViewPolicy(viewIds = {"myec_MainView", "myec_Trader.list", "myec_BankAccount.list", "myec_BankAccount.detail", "flowui_AddConditionView", "flowui_GroupFilterCondition.detail", "flowui_PropertyFilterCondition.detail", "flowui_JpqlFilterCondition.detail", "myec_BankTransaction.list", "myec_BankTransaction.detail", "myec_ExpenseInvoice.list", "myec_ExpenseInvoice.detail", "myec_ViewTraderBalances.list", "myec_SupplierInfoView", "myec_TotalsView"})
     void main();
 
     @ViewPolicy(viewIds = "myec_LoginView")
@@ -36,7 +36,7 @@ public interface UiMinimalRole {
     @EntityAttributePolicy(entityClass = KeyValueEntity.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
     void keyValueEntity();
 
-    @MenuPolicy(menuIds = {"myec_Trader.list", "myec_BankAccount.list", "myec_BankTransaction.list", "myec_ExpenseInvoice.list", "myec_ViewTraderBalances.list", "myec_SupplierInfoView"})
+    @MenuPolicy(menuIds = {"myec_Trader.list", "myec_BankAccount.list", "myec_BankTransaction.list", "myec_ExpenseInvoice.list", "myec_ViewTraderBalances.list", "myec_SupplierInfoView", "myec_TotalsView"})
     void screens();
 
     @EntityAttributePolicy(entityClass = Trader.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
---
+++ src/main/java/gr/maravelias/myeconomics/view/totals/TotalsView.java
@@ -0,0 +1,14 @@
+package gr.maravelias.myeconomics.view.totals;
+
+
+import com.vaadin.flow.router.Route;
+import gr.maravelias.myeconomics.view.main.MainView;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route(value = "totals-view", layout = MainView.class)
+@ViewController(id = "myec_TotalsView")
+@ViewDescriptor(path = "totals-view.xml")
+public class TotalsView extends StandardView {
+}
